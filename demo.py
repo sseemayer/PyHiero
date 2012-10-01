11d6d5e3041420460c1b241b1555b77386b31c97
Stefan Seemayer <stefan@seemayer.de>
2012-10-02 00:20:45 +0200

    Finished first version
    + separated parser from rendering implementation
    + add pygame bitmap font class

--- demo.py
+++ demo.py
@@ -1,5 +1,32 @@
-from pyhiero.font import HieroFont
+from pyhiero.pygamefont import PyGameHieroFont
 
-x = HieroFont("examples/Oxygen/Oxygen.fnt")
 
-import pdb; pdb.set_trace()
+
+import pygame, sys
+from pygame.locals import *
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((400, 300))
+
+pygame.display.set_caption('Hello World!')
+
+
+fnt = PyGameHieroFont("examples/CabinSketch/CabinSketch.fnt")
+
+txt = fnt.render("Hello World! *g* - semi", color=(128,0,0))
+
+while True: 
+	for event in pygame.event.get():
+
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+
+	screen.fill((255,255,255))
+
+	screen.blit(txt, (0,0))
+
+	pygame.display.update()
+
